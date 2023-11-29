70e31b9c9b85984d175c430bfdb1e579b7ef6d08
betossauro <pfvrbeto@gmail.com>
2023-11-29 18:36:01 -0300

    adição de icones, construção em andamento de registro de serviços e navbar

--- src/app/servicos/servicos-routing.module.ts
+++ src/app/servicos/servicos-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ServicoCadastroComponent } from './servico-cadastro/servico-cadastro.component';
+import { ServicoOcorrenciaComponent } from './servico-ocorrencia/servico-ocorrencia.component';
+import { ServicoListagemGerenciaComponent } from './servico-listagem-gerencia/servico-listagem-gerencia.component';
+import { ServicoListagemFuncionarioComponent } from './servico-listagem-funcionario/servico-listagem-funcionario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cadastro', component: ServicoCadastroComponent },
+  { path: 'listagem/funcionario', component: ServicoListagemFuncionarioComponent },
+  { path: 'listagem/gerencia', component: ServicoListagemGerenciaComponent },
+  { path: 'ocorrencia', component: ServicoOcorrenciaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
